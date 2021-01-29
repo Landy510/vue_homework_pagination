9a52eee52869f91cf2fe72b4610030fd503b95bd
Landy510 <randychen510@gmail.com>
2021-01-29 15:43:14 +0800

    修改一些bootstrap的scss樣式

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         
-        <header class="navbar navbar-dark sticky-top bg-yl flex-md-nowrap p-0 shadow">
+        <header class="navbar navbar-dark sticky-top bg-warning flex-md-nowrap p-0 shadow">
             <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3  text-dark font-weight-bold" href="#">
             <img src=".././assets/protectionshield.svg" width="50" height="50" class="d-inline-block" alt="">
             Berserker Fitness
--- src/components/pages/Order.vue
+++ src/components/pages/Order.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
         
+        <table class="table mt-4">
+            <thead>
+                <th width="120">分類</th>
+                <th>產品名稱</th>
+                <th width="120">原價</th>
+                <th width="120">售價</th>
+                <th width="100">是否啟用</th>
+                <th width="80">編輯</th>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+        
+        
         暫定訂單列表的頁面
     </div>
 </template>
@@ -19,12 +34,16 @@
                 const vm = this;
                 
                 const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/orders?page=${page}`;
+                
                 this.$http.get(api).then((response) => {
-                   
-                    console.log('你好',response.data);
+                    if(response.data.success){
+                        
+                        console.log('response',response.data.success);
+                    }
                     
-                    console.log('isLoad',vm.isLoad);
+                    console.log('response',response.data.success);
                 })
+                
             }
         },
         created(){
