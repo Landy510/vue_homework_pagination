7fa14890f45474bbf79944bb46e018511d6776b1
Landy510 <randychen510@gmail.com>
2021-01-29 16:02:52 +0800

    新增接收列表的功能

--- src/components/pages/Order.vue
+++ src/components/pages/Order.vue
@@ -3,45 +3,58 @@
         
         <table class="table mt-4">
             <thead>
-                <th width="120">分類</th>
-                <th>產品名稱</th>
-                <th width="120">原價</th>
-                <th width="120">售價</th>
-                <th width="100">是否啟用</th>
-                <th width="80">編輯</th>
+                <th width="120">購買名稱</th>
+                <th width="120">Email</th>
+                <th width="120">購買款項</th>
+                <th width="100">應付金額</th>
+                <th width="80">是否付款</th>
             </thead>
             <tbody>
-                
+                <tr v-for="(item, key) in product" :key="key">
+                    <td>{{item.create_at}}</td>
+                    <td>{{item.user.eamil}}</td>
+                    <td>{{item.products}}</td>
+                    
+                    <td class="text-right">
+                        {{item.total | currency}}
+                    </td>
+                    <td>
+                        <span v-if="item.is_paid" class="text-success">已付款</span>
+                        <span v-else>未付款</span>
+                    </td>
+                    
+                </tr>
             </tbody>
         </table>
-        
-        
-        暫定訂單列表的頁面
+        <pagination :has_pre="pagination.has_pre" :has_next="pagination.has_next" :total_page="pagination.total_pages" :current_page="pagination.current_page" v-on:increment="getList"></pagination>
     </div>
 </template>
 
 
 <script>
     import $ from 'jquery';
+    import pagination from './pagination';
 
     export default{
         data(){
-            return{}
+            return{
+                product:[],
+                pagination:{},
+            }
+        },
+        components:{
+            pagination
         },
         methods:{
             getList(page = 1){
                 console.log('page',page);
                 const vm = this;
-                
                 const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/orders?page=${page}`;
-                
                 this.$http.get(api).then((response) => {
                     if(response.data.success){
-                        
-                        console.log('response',response.data.success);
+                        vm.product = response.data.orders;
+                        vm.pagination = response.data.pagination;
                     }
-                    
-                    console.log('response',response.data.success);
                 })
                 
             }
